f51407fafe751a800240b6a9377d5e87195dff4c
crist1x <roman.yashin.07@yandex.ru>
2026-06-14 22:58:24 +0300

    LAST

--- pages/1_Heart.py
+++ pages/1_Heart.py
@@ -7,8 +7,6 @@ st.set_page_config(page_title="Heart", layout="wide")
 st.title("Heart Animation")
 st.caption("3D model of a beating heart with the sound of a heartbeat")
 
-st.audio("Heart animation/stuk_serdca_-_zvuk_serdcebieniya.mp3", autoplay=True)
-
 heart_path = Path(__file__).parent.parent / "Heart animation" / "heart.html"
 heart_html = heart_path.read_text(encoding="utf-8")
 
