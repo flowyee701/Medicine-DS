328ade909f790a692927fa90cc6b08a3167390d2
crist1x <roman.yashin.07@yandex.ru>
2026-06-14 20:23:27 +0300

    Almost final

--- pages/hypo3.py
+++
@@ -1,2 +0,0 @@
-from main_page import show_chart
-show_chart("/hypo3", "Hypothesis 3: Bazette vs Fridericia")
--- pages/polar.py
+++
@@ -1,2 +0,0 @@
-from main_page import show_chart
-show_chart("/elaxis", "Электрическая ось vs QRS")
